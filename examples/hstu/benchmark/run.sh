c7cc491ec7ff2218c047dad95f8ef276ebbe050e
junzhang <junzhang@nvidia.com>
2025-04-23 07:04:16 +0000

    Add run test script

---
+++ examples/hstu/benchmark/run.sh
@@ -0,0 +1,68 @@
+#!/bin/bash
+gpu_arch=$(nvidia-smi -L |head -n 1| cut -d' ' -f4)
+PROFILE=${1:-0}
+nsys_profile_args='-f true -s none -t cuda,nvtx -c cudaProfilerApi --cpuctxsw none --cuda-flush-interval 100 --capture-range-end=stop --cuda-graph-trace=node'
+
+# dim_per_head=(64 128 256)
+# num_heads=(1 4 8) 
+# max_seqlen=(512 1024 2048 4096)
+# batchsize=(32 64 128)
+
+dim_per_heads=(128 256)
+num_heads=(8 )
+max_seqlens=(1024 2048)
+batchsizes=(32 )
+
+profiler_start=20
+profiler_end=40
+
+for dim_per_head in ${dim_per_heads[@]}; do
+    for num_head in ${num_heads[@]}; do
+        for max_seqlen in ${max_seqlens[@]}; do
+            for batchsize in ${batchsizes[@]}; do
+
+                echo "==== dim_per_head: $dim_per_head, num_heads: $num_head, max_seqlen: $max_seqlen, batchsize: $batchsize ==== "
+                native_output_profile_name=${gpu_arch}_native_bs${batchsize}_dim${dim_per_head}_heads${num_head}_seqlen${max_seqlen}
+                fused_output_profile_name=${gpu_arch}_fused_bs${batchsize}_dim${dim_per_head}_heads${num_head}_seqlen${max_seqlen}
+
+                if [ $PROFILE -eq 1 ]; then
+                    fused_nsys_cmd="nsys profile -o ./profile/fused_op/${fused_output_profile_name} ${nsys_profile_args}"
+                    native_nsys_cmd="nsys profile -o ./profile/fused_op/${native_output_profile_name} ${nsys_profile_args}"
+                else
+                    fused_nsys_cmd=""
+                    native_nsys_cmd=""
+                fi
+
+                ${fused_nsys_cmd} \
+                    python ./benchmark/fused_hstu_layer_benchmark.py run \
+                    --iters 100 \
+                    --warmup-iters 50 \
+                    --layer-type fused \
+                    --kernel-backend cutlass \
+                    --dim-per-head $dim_per_head \
+                    --num-heads $num_head \
+                    --dtype bfloat16 \
+                    --max-seqlen $max_seqlen \
+                    --batchsize $batchsize \
+                    --profiler-start $profiler_start \
+                    --profiler-end $profiler_end | tee ./profile/fused_op/${gpu_arch}_${fused_output_profile_name}.log
+
+                sleep 1
+                ${native_nsys_cmd} \
+                    python ./benchmark/fused_hstu_layer_benchmark.py run \
+                    --iters 100 \
+                    --warmup-iters 50 \
+                    --layer-type native \
+                    --kernel-backend cutlass \
+                    --dim-per-head $dim_per_head \
+                    --num-heads $num_head \
+                    --dtype bfloat16 \
+                    --max-seqlen $max_seqlen \
+                    --batchsize $batchsize \
+                    --profiler-start $profiler_start \
+                    --profiler-end $profiler_end | tee ./profile/fused_op/${gpu_arch}_${native_output_profile_name}.log
+                  sleep 1
+            done
+        done
+    done
+done
