969a2cf4d857522a445e38d4849495df8e76da3f
junzhang <junzhang@nvidia.com>
2025-06-18 06:56:06 +0000

    Rename logging -> logger and enhance nvtx_hook_op

--- examples/hstu/modules/hstu_block.py
+++ examples/hstu/modules/hstu_block.py
@@ -3,6 +3,7 @@
 from typing import Dict, Optional, Union
 
 import torch
+from commons.utils.nvtx_op import output_nvtx_hook
 from configs.hstu_config import HSTUConfig, HSTULayerType
 from dataset.utils import RankingBatch, RetrievalBatch
 from megatron.core.transformer.module import MegatronModule
@@ -57,7 +58,7 @@ class HSTUBlock(MegatronModule):
             [HSTULayerImpl(config) for l in range(self.config.num_layers)]
         )
 
-    # @output_nvtx_hook(nvtx_tag="hstu_preprocess")
+    @output_nvtx_hook(nvtx_tag="HSTUBlock preprocess", hook_key_or_attr_name="values")
     def hstu_preprocess(
         self, embeddings: Dict[str, JaggedTensor], batch: RankingBatch
     ) -> JaggedData:
@@ -181,7 +182,7 @@ class HSTUBlock(MegatronModule):
             has_interleaved_action=batch.action_feature_name is not None,
         )
 
-    # @output_nvtx_hook(nvtx_tag="hstu_postprocess")
+    @output_nvtx_hook(nvtx_tag="HSTUBlock postprocess", hook_key_or_attr_name="values")
     def hstu_postprocess(self, jd: JaggedData) -> JaggedData:
         """
         Postprocess the output from the HSTU architecture.
@@ -238,7 +239,7 @@ class HSTUBlock(MegatronModule):
             has_interleaved_action=False,
         )
 
-    # @output_nvtx_hook(nvtx_tag="HSTUBlock", hook_tensor_attr_name="values")
+    @output_nvtx_hook(nvtx_tag="HSTUBlock", hook_key_or_attr_name="values")
     def forward(
         self,
         embeddings: Dict[str, JaggedTensor],
