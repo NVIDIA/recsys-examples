0cab0335f92652adf8b486c5fa6cba01c700d722
aleliu <aleliu@nvidia.com>
2025-07-09 21:23:34 -0700

    add preprocessing mlp for hstu

--- examples/hstu/modules/hstu_block.py
+++ examples/hstu/modules/hstu_block.py
@@ -9,14 +9,12 @@ from dataset.utils import RankingBatch, RetrievalBatch
 from megatron.core.transformer.module import MegatronModule
 from modules.fused_hstu_layer import FusedHSTULayer
 from modules.jagged_data import JaggedData
+from modules.mlp import MLP
 from modules.native_hstu_layer import HSTULayer
 from modules.position_encoder import HSTUPositionalEncoder
 from ops.cuda_ops.JaggedTensorOpFunction import jagged_2D_tensor_concat
 from ops.length_to_offsets import length_to_complete_offsets
-from ops.triton_ops.triton_jagged import (  # type: ignore[attr-defined]
-    triton_concat_2D_jagged,
-    triton_split_2D_jagged,
-)
+from ops.triton_ops.triton_jagged import triton_split_2D_jagged
 from torchrec.sparse.jagged_tensor import JaggedTensor
 
 
@@ -38,6 +36,24 @@ class HSTUBlock(MegatronModule):
             self._training_dtype = torch.bfloat16
         if self.config.fp16:
             self._training_dtype = torch.float16
+
+        self._item_mlp = None
+        self._contextual_mlp = None
+        if config.hstu_preprocessing_config is not None:
+            self._item_mlp = MLP(
+                in_size=config.hstu_preprocessing_config.item_embedding_dim,
+                layer_sizes=[config.hidden_size, config.hidden_size],
+                activation="relu",
+                bias=True,
+                device=self.device,
+            )
+            self._contextual_mlp = MLP(
+                in_size=config.hstu_preprocessing_config.contextual_embedding_dim,
+                layer_sizes=[config.hidden_size, config.hidden_size],
+                activation="relu",
+                bias=True,
+                device=self.device,
+            )
 
         self._positional_encoder: Optional[HSTUPositionalEncoder] = None
         if config.position_encoding_config is not None:
@@ -96,6 +112,8 @@ class HSTUBlock(MegatronModule):
                 sequence_embeddings_lengths_offsets * 2
             )
             sequence_max_seqlen = sequence_max_seqlen * 2
+        if self._item_mlp is not None:
+            sequence_embeddings = self._item_mlp(sequence_embeddings)
 
         if batch.num_candidates is not None and batch.action_feature_name is not None:
             num_candidates = batch.num_candidates * 2
@@ -115,36 +133,40 @@ class HSTUBlock(MegatronModule):
             contextual_jts = [
                 embeddings[name] for name in batch.contextual_feature_names
             ]
-            all_values = [jt.values() for jt in contextual_jts] + [sequence_embeddings]
-            all_offsets = [jt.offsets() for jt in contextual_jts] + [
-                sequence_embeddings_lengths_offsets
-            ]
-            all_max_seqlens = contextual_max_seqlens + [sequence_max_seqlen]
+            contextual_jts_values = [jt.values() for jt in contextual_jts]
+            contextual_jts_offsets = [jt.offsets() for jt in contextual_jts]
             (
-                sequence_embeddings,
-                sequence_embeddings_lengths_after_concat,
+                contextual_sequence_embeddings,
+                contextual_seqlen,
             ) = jagged_2D_tensor_concat(
-                all_values,
-                all_offsets,
-                all_max_seqlens,
+                contextual_jts_values,
+                contextual_jts_offsets,
+                contextual_max_seqlens,
+            )
+            if self._contextual_mlp is not None:
+                contextual_sequence_embeddings = self._contextual_mlp(
+                    contextual_sequence_embeddings
+                )
+            contextual_seqlen_offsets = torch.ops.fbgemm.asynchronous_complete_cumsum(
+                contextual_seqlen
             )
             contextual_max_seqlen = max(
                 len(batch.contextual_feature_names), sum(contextual_max_seqlens)
             )
 
-            contextual_seqlen = (
-                sequence_embeddings_lengths_after_concat - sequence_embeddings_lengths
+            (
+                sequence_embeddings,
+                sequence_embeddings_lengths,
+            ) = jagged_2D_tensor_concat(
+                [contextual_sequence_embeddings, sequence_embeddings],
+                [contextual_seqlen_offsets, sequence_embeddings_lengths_offsets],
+                [contextual_max_seqlen, sequence_max_seqlen],
             )
-            sequence_embeddings_lengths = sequence_embeddings_lengths_after_concat
 
             sequence_embeddings_lengths_offsets = (
                 torch.ops.fbgemm.asynchronous_complete_cumsum(
                     sequence_embeddings_lengths
                 )
-            )
-
-            contextual_seqlen_offsets = torch.ops.fbgemm.asynchronous_complete_cumsum(
-                contextual_seqlen
             )
 
             sequence_max_seqlen = sequence_max_seqlen + contextual_max_seqlen
