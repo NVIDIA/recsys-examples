e7690ca654f94338f3713c70fab00efca28be18e
jiashuy <jiashu.yao.cn@gmail.com>
2025-12-04 09:33:49 +0800

    Counter table interface and ScoredHashTable Implementation (#229)
    * Admission counter table interface.
    
    * Counter table implementation
    
    * Unit test and Fix IMA of table operations
    
    * Unit test of table.dump and table.load
    
    * Add table operation unit tests to CI
    
    * Unit test of table.dump&load when num_gpu mismatched.
    
    * Unit test to table.insert_and_evict.
    
    * Add todo to unlock using index.
    
    * Remove kvcounter in dynamicemb_table_v2
    
    * Refine unit test of load and dump APIs of ScoredHashTable
    
    * Fix potential issues and rigorously test the score in test_embedding_dump_load.py

--- corelib/dynamicemb/test/unit_tests/test_embedding_dump_load.py
+++ corelib/dynamicemb/test/unit_tests/test_embedding_dump_load.py
@@ -185,13 +185,84 @@ def init_fn(x: torch.Tensor):
         x.fill_(2.0)
 
 
+def update_scores(
+    score_strategy: str,
+    expect_scores: Dict[int, int],
+    key: int,
+    step: int,
+):
+    if score_strategy == "step":
+        expect_scores[key] = step
+    elif score_strategy == "lfu":
+        if key not in expect_scores:
+            expect_scores[key] = 1
+        else:
+            expect_scores[key] = expect_scores[key] + 1
+    else:
+        return
+
+
 def generate_sparse_feature(
-    feature_num, num_embeddings_list, multi_hot_sizes, local_batch_size=50
+    num_embedding_collections: int,
+    num_embeddings: List[int],
+    multi_hot_sizes: List[int],
+    rank: int,
+    world_size: int,
+    batch_size: int,
+    num_iterations: int,
+    score_strategy: str,
+    scores_collection: Dict[str, Dict[int, int]],
+    seed: int = 42,
 ):
     feature_batch = feature_num * local_batch_size
 
-    indices = []
-    lengths = []
+    batch_size_per_rank = batch_size // world_size
+    kjts = []
+    all_kjts = []
+    for embedding_collection_id in range(num_embedding_collections):
+        for embedding_id, _ in enumerate(num_embeddings):
+            _, table_name = idx_to_name(embedding_collection_id, embedding_id)
+            scores_collection[table_name] = {}
+    step = 0
+    for _ in range(num_iterations):
+        step += 1
+        cur_indices, cur_lengths = [], []
+        all_indices, all_lengths = [], []
+        keys = []
+        for embedding_collection_id in range(num_embedding_collections):
+            for embedding_id, num_embedding in enumerate(num_embeddings):
+                feature_name, table_name = idx_to_name(
+                    embedding_collection_id, embedding_id
+                )
+                expected_scores: Dict[int, int] = scores_collection[table_name]
+                for sample_id in range(batch_size):
+                    hotness = random.randint(
+                        0, multi_hot_sizes[embedding_collection_id]
+                    )
+                    indices = [random.randint(0, (1 << 63) - 1) for _ in range(hotness)]
+                    all_indices.extend(indices)
+                    all_lengths.append(hotness)
+                    if sample_id // batch_size_per_rank == rank:
+                        cur_indices.extend(indices)
+                        cur_lengths.append(hotness)
+                    for index in indices:
+                        update_scores(score_strategy, expected_scores, index, step)
+                keys.append(feature_name)
+        kjts.append(
+            KeyedJaggedTensor.from_lengths_sync(
+                keys=keys,
+                values=torch.tensor(cur_indices, dtype=torch.int64).cuda(),
+                lengths=torch.tensor(cur_lengths, dtype=torch.int64).cuda(),
+            )
+        )
+        all_kjts.append(
+            KeyedJaggedTensor.from_lengths_sync(
+                keys=keys,
+                values=torch.tensor(all_indices, dtype=torch.int64).cuda(),
+                lengths=torch.tensor(all_lengths, dtype=torch.int64).cuda(),
+            )
+        )
+    return kjts, keys, all_kjts
 
     for i in range(feature_batch):
         f = i // local_batch_size
@@ -658,12 +729,177 @@ def main(argv: List[str]) -> None:
     elif args.score_type == "custimized":
         args.score_strategies = DynamicEmbScoreStrategy.CUSTOMIZED
 
-    # Print all arguments
-    print("Arguments:")
-    for arg, value in vars(args).items():
-        print(f"{arg}: {value}")
+@click.command()
+@click.option("--num-embedding-collections", type=int, required=True)
+@click.option("--num-embeddings", type=str, required=True)
+@click.option("--multi-hot-sizes", type=str, required=True)
+@click.option("--embedding-dim", type=int, required=True)
+@click.option("--save-path", type=str, required=True)
+@click.option(
+    "--optimizer-type",
+    type=click.Choice(["sgd", "adam", "adagrad", "rowwise_adagrad"]),
+    required=True,
+)
+@click.option("--mode", type=click.Choice(["load", "dump"]), required=True)
+@click.option(
+    "--score-strategy",
+    type=click.Choice(["timestamp", "step", "lfu"]),
+    required=True,
+)
+@click.option("--optim", type=bool, required=True)
+def test_model_load_dump(
+    num_embedding_collections: int,
+    num_embeddings: str,
+    multi_hot_sizes: str,
+    embedding_dim: int,
+    optimizer_type: str,
+    score_strategy: str,
+    mode: str,
+    save_path: str,
+    optim: bool,
+    batch_size: int = 128,
+    num_iterations: int = 10,
+):
+    rank = int(os.environ["LOCAL_RANK"])
+    world_size = int(os.environ["WORLD_SIZE"])
 
-    run(args)
+    num_embeddings = [int(v) for v in num_embeddings.split(",")]
+    multi_hot_sizes = [int(v) for v in multi_hot_sizes.split(",")]
+
+    for num_embedding, multi_hot_size in zip(num_embeddings, multi_hot_sizes):
+        if batch_size * num_iterations * multi_hot_size > num_embedding:
+            raise ValueError(
+                "batch_size * num_iterations * multi_hot_size > num_embedding, this may lead to eviction of dynamicemb and cause test fail"
+            )
+
+    optimizer_kwargs = get_optimizer_kwargs(optimizer_type)
+    score_strategy_ = get_score_strategy(score_strategy)
+
+    ref_model = create_model(
+        num_embedding_collections=num_embedding_collections,
+        num_embeddings=num_embeddings,
+        embedding_dim=embedding_dim,
+        optimizer_kwargs=optimizer_kwargs,
+        score_strategy=score_strategy_,
+    )
+
+    expect_scores_collection: Dict[str, Dict[int, int]] = {}
+    kjts, feature_names, all_kjts = generate_sparse_feature(
+        num_embedding_collections=num_embedding_collections,
+        num_embeddings=num_embeddings,
+        multi_hot_sizes=multi_hot_sizes,
+        rank=dist.get_rank(),
+        world_size=dist.get_world_size(),
+        batch_size=batch_size,
+        num_iterations=num_iterations,
+        score_strategy=score_strategy,
+        scores_collection=expect_scores_collection,
+    )
+
+    for kjt in kjts:
+        ret = ref_model(kjt)
+        loss = (
+            ret.sum() * dist.get_world_size()
+        )  # scale the loss by world size to make the gradients consistent between different gpu settings
+        loss.backward()
+
+    if mode == "dump":
+        shutil.rmtree(save_path, ignore_errors=True)
+        DynamicEmbDump(save_path, ref_model, optim=optim)
+
+    if mode == "load":
+        model = create_model(
+            num_embedding_collections=num_embedding_collections,
+            num_embeddings=num_embeddings,
+            embedding_dim=embedding_dim,
+            optimizer_kwargs=optimizer_kwargs,
+            score_strategy=score_strategy_,
+        )
+
+        DynamicEmbLoad(save_path, model, optim=optim)
+
+        table_name_to_key_score_dict = {}
+        for _, _, sharded_module in find_sharded_modules(model):
+            dynamic_emb_modules = get_dynamic_emb_module(sharded_module)
+            for dynamic_emb_module in dynamic_emb_modules:
+                for table_name, table in zip(
+                    dynamic_emb_module.table_names, dynamic_emb_module.tables
+                ):
+                    key_to_score = {}
+                    for batched_key, _, _, batched_score in batched_export_keys_values(
+                        table.table, torch.device(f"cpu")
+                    ):
+                        for key, score in zip(
+                            batched_key.tolist(), batched_score.tolist()
+                        ):
+                            key_to_score[key] = score
+                    table_name_to_key_score_dict[table_name] = key_to_score
+
+        for embedding_collection_idx, embedding_idx in product(
+            range(num_embedding_collections), range(len(num_embeddings))
+        ):
+            feature_name, table_name = idx_to_name(
+                embedding_collection_idx, embedding_idx
+            )
+            key_to_score_dict = table_name_to_key_score_dict[table_name].copy()
+            expect_scores = expect_scores_collection[table_name]
+
+            if score_strategy == "step" or score_strategy == "lfu":
+                for kjt in reversed(all_kjts):
+                    keys = kjt[feature_name].values().tolist()
+                    for key in keys:
+                        if key % world_size == rank:
+                            assert (
+                                key in key_to_score_dict
+                            ), f"Key {key} must exist in table of rank {rank}."
+                            assert (
+                                key_to_score_dict[key] == expect_scores[key]
+                            ), f"Expect {key_to_score_dict[key]} = {expect_scores[key]}"
+            # The idea is that the score of a newer key is greater than that of an older key. Therefore, I iterate through the input in reverse order and track the minimum score encountered. For each batch, the score should be lower than the minimum score from the previous batch. To avoid issues caused by duplicate keys, every time I access a key, I set its score to -inf. This ensures that if that key appears again, its score will be sufficiently small to remain below the minimum score.
+            elif score_strategy == "timestamp":
+                visited_keys = set({})
+                min_score = float("inf")
+                lasted_min_score = float("inf")
+                for kjt in reversed(all_kjts):
+                    keys = kjt[feature_name].values().tolist()
+                    for key in keys:
+                        if key % world_size == rank:
+                            assert (
+                                key in key_to_score_dict
+                            ), f"Key {key} must exist in table of rank {rank}."
+                        else:
+                            continue
+
+                        if key not in visited_keys:
+                            assert (
+                                key_to_score_dict[key] <= min_score
+                            ), f"key {key} score {key_to_score_dict[key]} should be < min_score {min_score}"
+                            lasted_min_score = min(
+                                lasted_min_score, key_to_score_dict[key]
+                            )
+                            visited_keys.add(key)
+
+                    min_score = lasted_min_score
+                    lasted_min_score = min_score
+
+            else:
+                raise RuntimeError("Not supported score strategy.")
+
+        if optim:
+            for kjt in kjts:
+                ret = model(kjt)
+                ret.sum().backward()
+                ref_ret = ref_model(kjt)
+                ref_ret.sum().backward()
+
+        ref_model = ref_model.eval()
+        model = model.eval()
+
+        with torch.inference_mode():
+            for kjt in kjts:
+                ret = model(kjt)
+                ref_ret = ref_model(kjt)
+                assert torch.allclose(ret, ref_ret)
 
 
 if __name__ == "__main__":
